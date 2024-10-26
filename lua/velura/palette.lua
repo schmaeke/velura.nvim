5478551d8f3d4328779a7122a05be7371d90b42c
schmaeke <jan@schmaeke.dev>
2024-10-26 15:02:40 +0200

    Removed old commented colors

--- lua/velura/palette.lua
+++ lua/velura/palette.lua
@@ -5,50 +5,6 @@ local M = { }
 M.default = {
 	none = "NONE",
 
-	--[[
-	-- background color
-	bg0 = "#1E1516",
-	bg1 = "#2E1F21",
-	bg2 = "#3D292C",
-	bg3 = "#4C3337",
-
-	-- foreground color
-	fg0 = "#D6C2C5",
-	fg1 = "#E0D1D4",
-	fg2 = "#EBE0E2",
-	fg3 = "#F5F0F1",
-
-	-- reds
-	red0 = "#C5223D",
-	red1 = "#E02040",
-	red2 = "#EB4763",
-
-	-- greens
-	green0 = "#30A64A",
-	green1 = "#30C050",
-	green2 = "#4AD96A",
-
-	-- yellows
-	yellow0 = "#E9C63E",
-	yellow1 = "#F0D050",
-	yellow2 = "#F8DF7C",
-
-	-- blues
-	blue0 = "#2E94E8",
-	blue1 = "#40A0F0",
-	blue2 = "#6BB8F8",
-
-	-- purple
-	purple0 = "#A43EE9",
-	purple1 = "#B050F0",
-	purple2 = "#C67CF8",
-
-	-- cyan
-	cyan0 = "#62B8A9",
-	cyan1 = "#6EC5B6",
-	cyan2 = "#8FD7CB",
-	]]--
-
 	-- background color
 	bg0 = "#1A1416",   -- Darker to provide contrast with text
 	bg1 = "#2B1D20",   -- Slightly less red, more neutral
@@ -90,7 +46,6 @@ M.default = {
 	cyan0 = "#58AA9A",   -- Muted for balance with greens
 	cyan1 = "#66B8A7",   -- Darkened for less dominance
 	cyan2 = "#80C9BC",   -- Lighter, for clearer contrast in highlights
-
 }
 
 return M
