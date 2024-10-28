721987b2dd0758195b0d8ea4055fbc576c5051e6
schmaeke <jan@schmaeke.dev>
2024-10-28 14:22:44 +0100

    Added colors for diagnostics

--- lua/velura/util.lua
+++ lua/velura/util.lua
@@ -7,9 +7,10 @@ local function apply_group( group, props )
 	local bg = props.bg == nil and "" or "guibg = " .. props.bg
 	local fg = props.fg == nil and "" or "guifg = " .. props.fg
 	local style = props.style == nil and "" or "gui = " .. props.style
+	local sp = props.sp == nil and "" or "guisp = " .. props.sp
 
 	local cmd = table.concat( {
-		"highlight", group, bg, fg, style
+		"highlight", group, bg, fg, sp, style
 	}, " ")
 
 	vim.api.nvim_command( cmd )
