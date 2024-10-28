2288744894799d2f9bb711bf1cf20aa358e1447d
schmaeke <jan@schmaeke.dev>
2024-10-28 22:11:16 +0100

    Changed the border color of telescope

--- lua/velura/telescope.lua
+++ lua/velura/telescope.lua
@@ -1,8 +1,8 @@
 telescope = {
 	TelescopeNormal = { fg = base.fg2, bg = base.bg0 },
-	TelescopeSelection = { fg = base.fg2, bg = base.bg3 },
+	TelescopeSelection = { fg = base.fg2, bg = base.bg2 },
 	TelescopeMatching = { fg = accents.yellow2 },
-	TelescopeBorder = { fg = base.fg3, bg = base.bg0 },
+	TelescopeBorder = { fg = base.fg0, bg = base.bg0 },
 }
 
 return telescope
